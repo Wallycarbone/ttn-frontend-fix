d9fc10a7f5073a540bd46701c7055ae3f72d8dc3
Wallycarbone <wallycarbone@gmail.com>
2025-05-05 00:07:19 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000,
-    host: '0.0.0.0'
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
-  }
-})
+});
